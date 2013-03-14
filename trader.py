4b3a93415e43b2d332562944c4b4b9cd32ce2df6
Nicholas James Grasevski <nicholasg@cse.unsw.EDU.AU>
2013-03-14 17:56:23 +1100

    Changing skeleton file after email from the lecturer, this will probably change again soon

--- trader.py
+++ trader.py
@@ -1,32 +1,28 @@
 """Trader module, containing the main logic"""
 
-def runTrial(strategy, testDataCsv):
-    """Run the strategy against the test data and report the results"""
-    class Engine:
-        """Trader Engine"""
-        def addOrder(self, order):
-            """Process a single order and return a list of resultant trades"""
-            raise NotImplementedError
-
-        def addOrders(self, orders):
-            """Process multiple orders and return a list of resultant trades"""
-            trades = []
-            for order in orders:
-                trades.extend(addOrder(order))
-            return trades
-
-    def signalGenerator(testDataCsv):
-        """Process the file and return a list of orders"""
+class SignalGenerator:
+    def __call__(self, marketData):
         raise NotImplementedError
 
-    def strategyEvaluator(orders, trades):
-        """Analyze the orders and trades and output the evaluation of the strategy"""
+class Engine:
+    def addOrder(self, order):
         raise NotImplementedError
 
+    def addOrders(self, orders):
+        trades = []
+        for order in orders:
+            trades.extend(addOrder(order))
+        return trades
+
+class StrategyEvaluator:
+    def __call__(self, trades):
+        raise NotImplementedError
+
+def runTrial(marketData):
+    signalGenerator = SignalGenerator()
     engine = Engine()
-    orders = signalGenerator(testDataCsv)
+    strategyEvaluator = StrategyEvaluator()
     trades = []
-    for order in orders:
+    for order in signalGenerator(marketData):
         trades.extend(engine.addOrder(order))
-        trades.extend(engine.addOrders(strategy(order)))
-    strategyEvaluator(orders, trades)
+    strategyEvaluator(trades)
