3fee765783a862aa3d31c54fcbbe2221bedc942e
Nicholas Grasevski <grasevski@gmail.com>
2013-03-14 23:30:58 +1100

    Cleaned up code skeleton, now hopefully all that needs to be done is fill in the stubs

--- trader.py
+++ trader.py
@@ -2,28 +2,25 @@
 
 import sys
 
+def signal_generator(market_data):
+    """Reads market data and generates trading signals"""
+    raise NotImplementedError
+
+class Engine:
+    """Implements the market rules"""
+
+    def __init__(self):
+        raise NotImplementedError
+
+    def __call__(self, order):
+        raise NotImplementedError
+
+def strategy_evaluator(trades):
+    """Gives some sort of payoff from using the trading strategy"""
+    raise NotImplementedError
+
 def run_trial(market_data):
     """Run the experiment with the given market data"""
-
-    def signal_generator(market_data):
-        """Reads market data and generates trading signals"""
-        raise NotImplementedError
-
-    class Engine:
-        """Implements the market rules"""
-
-        def __init__(self):
-            """Initialize the required data structures"""
-            raise NotImplementedError
-
-        def __call__(self, order):
-            """Generates trades from orders"""
-            raise NotImplementedError
-
-    def strategy_evaluator(trades):
-        """Gives some sort of payoff from using the trading strategy"""
-        raise NotImplementedError
-        
     engine = Engine()
     trades = []
     for order in signal_generator(market_data):
