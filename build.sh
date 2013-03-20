07e1d3b5debcb7f89ccbbb40ba24c715ce5bca3e
Nicholas Grasevski <grasevski@gmail.com>
2013-03-20 12:31:23 +1100

    Added report1 template

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+echo "aoeuaoeuaoeuaoeuaoeu"
 pycompile *.py && pydoc -w trader trader_cli test_trader && ./trader.py && ./test_trader.py && pdflatex -output-directory doc doc/report1.tex
+echo "aoeuaoeuaoeuaoeuaoeu"
