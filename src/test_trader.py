167c929e3667be0c0bb5d78a156b020647778567
Nicholas Grasevski <grasevski@gmail.com>
2013-03-20 14:07:26 +1100

    Rearranged directory structure

---
+++ src/test_trader.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+"""Trader unit test"""
+
+import trader
+import unittest
+
+class TestTrader(unittest.TestCase):
+    """Trader test case"""
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ src/trader_cli.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+"""Trader command line interface"""
+
+from trader import run_trial
+import sys
+
+def main():
+    """Run the trial, taking market data from stdin"""
+    run_trial(sys.stdin)
+
+if __name__ == '__main__':
+    main()
