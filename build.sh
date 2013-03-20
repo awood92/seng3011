969a023eee85e9e4a13c0384da4c1cb2bd8b9a1e
Nicholas Grasevski <grasevski@gmail.com>
2013-03-20 12:31:43 +1100

    Added report1 template

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-echo "aoeuaoeuaoeuaoeuaoeu"
 pycompile *.py && pydoc -w trader trader_cli test_trader && ./trader.py && ./test_trader.py && pdflatex -output-directory doc doc/report1.tex
-echo "aoeuaoeuaoeuaoeuaoeu"
--- pre-commit.sh
+++ pre-commit.sh
@@ -46,3 +46,5 @@ fi
 exec git diff-index --check --cached $against --
 
 ./build.sh
+
+echo "aoeuaoeuaoeuao"
