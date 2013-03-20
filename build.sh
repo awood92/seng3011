c433df8a9b7fafe6c38d2694fa94e39322d1639c
Nicholas Grasevski <grasevski@gmail.com>
2013-03-20 14:07:12 +1100

    Rearranged directory structure

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-pycompile *.py && pydoc -w trader trader_cli test_trader && ./trader.py && ./test_trader.py && pdflatex -output-directory doc doc/report1.tex
+cd src
+success=false
+if pycompile . && pydoc -w trader trader_cli test_trader && ./trader.py && ./test_trader.py
+then success=true
+fi
+cd ..
+if ! $success; then exit 1; fi
+pdflatex -output-directory doc doc/report1.tex
