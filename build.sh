4197d3c77ed86cefd46a465a80a0739e014f637f
Nicholas Grasevski <grasevski@gmail.com>
2013-03-28 12:45:41 +1100

    Fixed some typos in report

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ failure=$?
 cd ..
 if [ $failure -ne 0 ]; then exit $failure; fi
 cd doc
-lacheck report1.tex && pdflatex report1.tex
+lacheck report1.tex && pdflatex report1.tex && rm -f report1.zip && zip report1 report1.pdf
 failure=$?
 cd ..
 exit $failure
