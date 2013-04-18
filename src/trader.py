b6da50b90d7d54b31b7ffc051cc74c4b937364b9
Nicholas Grasevski <grasevski@gmail.com>
2013-04-18 10:25:27 +1000

    Added counter to heap priorities to keep them stable

--- src/trader.py
+++ src/trader.py
@@ -2,6 +2,7 @@
 
 """trader module, containing the main logic"""
 
+import itertools
 import heapq
 
 
@@ -10,17 +11,20 @@ def run_trial(market_data, signal_generator,
     """Run the experiment with the given market data"""
     trades = []
     orders = []
+    count = itertools.count()
     for order in signal_generator():
-        heapq.heappush(orders, ((order['Date'], order['Time']), order))
+        heapq.heappush(orders, (order['Date'], order['Time'], count, order))
+        next(count)
     for trading_record in market_data:
         engine_time = (trading_record['Date'], trading_record['Time'])
-        while len(orders) > 0 and orders[0][0] < engine_time:
-            trades.extend(engine(heapq.heappop(orders)[1]))
+        while len(orders) > 0 and orders[0][0:2] < engine_time:
+            trades.extend(engine(heapq.heappop(orders)[3]))
 	trades.extend(engine(trading_record))
         for order in signal_generator(trading_record):
-            heapq.heappush(orders, ((order['Date'], order['Time']), order))
+            heapq.heappush(orders, (order['Date'], order['Time'], count, order))
+            next(count)
     for i in range(len(orders)):
-        trades.extend(engine(heapq.heappop(orders)[1]))
+        trades.extend(engine(heapq.heappop(orders)[3]))
     for order in signal_generator():
         trades.extend(engine(order))
     strategy_evaluator(trades)
