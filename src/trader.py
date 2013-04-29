b8283aa8e98eb21da585a36a5f3ad4c48469bccd
George El Boustani <george_el_boustani@hotmail.com>
2013-04-30 01:37:56 +1000

    trader.py didnt push with previous commit, try number 2

--- src/trader.py
+++ src/trader.py
@@ -12,22 +12,38 @@ def run_trial(market_data, signal_generator,
     trades = []
     orders = []
     count = itertools.count()
+
+    # Get the signal generators initial orders i.e. before market opens
     for order in signal_generator():
         heapq.heappush(orders, (order['Date'], order['Time'], count, order))
         next(count)
+
     for trading_record in market_data:
-        engine_time = (trading_record['Date'], trading_record['Time'])
-        while len(orders) > 0 and orders[0][:2] < engine_time:
-            trades.extend(engine(heapq.heappop(orders)[3]))
-        trades.extend(engine(trading_record))
-        for order in signal_generator(trading_record):
-            heapq.heappush(orders,
-                           (order['Date'], order['Time'], count, order))
-            next(count)
+        recordType = trading_record['Record Type']
+        # We need to filter existing trades out, because signal generator now excepts trades
+        if recordType != 'TRADE' and 
+           recordType != 'CANCEL_TRADE' and 
+           recordType != 'OFFTR':
+            engine_time = (trading_record['Date'], trading_record['Time'])
+            while len(orders) > 0 and orders[0][:2] < engine_time:
+                trades.extend(engine(heapq.heappop(orders)[3]))
+            newtrades = engine(trading_record)
+            trades.extend(newtrades)
+            # Inform the signal generator about the new trades made
+            for newtrade in newtrades:
+                for order in signal_generator(newtrade):
+                    heapq.heappush(orders,
+                                   (order['Date'], order['Time'], count, order))
+                    next(count)        
+            # Inform the signal generator about the new orders placed
+            for order in signal_generator(trading_record):
+                heapq.heappush(orders,
+                               (order['Date'], order['Time'], count, order))
+                next(count)
     for i in range(len(orders)):
         trades.extend(engine(heapq.heappop(orders)[3]))
-    for order in signal_generator():
-        trades.extend(engine(order))
+    #for order in signal_generator():
+    #    trades.extend(engine(order))
     strategy_evaluator(trades)
     return trades
 
