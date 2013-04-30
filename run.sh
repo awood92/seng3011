112a48e15c1fb64c928e43c01b02b592c8c661ef
Daniel Morton <daniel@DAN-NETBOOK.(none)>
2013-04-30 19:45:19 +1000

    fixed run

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ cd src
 if [ $? -eq 2 ]; then exit 2; fi
 #pep8 .
 py_compilefiles *.py plugins/*.py && pydoc -w trader trader_cli test_trader plugins test_plugins && ./test_trader.py && ./test_plugins.py
-cat ../sample/1-march-2013-data.csv | ./trader_cli.py -e "Initial Engine" -s "Initial Strategy Evaluator" -g "Momentum Signal Generator" && python -m SimpleHTTPServer & xdg-open http://127.0.0.1:8000/SENG3011/src/ 
+cat ../sample/1-march-2013-data.csv | ./trader_cli.py -e "Initial Engine" -s "Initial Strategy Evaluator" -g "Momentum Signal Generator" && python -m SimpleHTTPServer & xdg-open http://127.0.0.1:8000/
 failure=$?
 cd ..
 if [ $failure -ne 0 ]; then exit $failure; fi
