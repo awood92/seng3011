d147e911ec5f50ba2bda30279e2dc189f98ea69c
Daniel Morton <daniel@DAN-NETBOOK.(none)>
2013-04-30 14:19:58 +1000

    fix again

--- src/plugins/InitialStrategyEvaluator.py
+++ src/plugins/InitialStrategyEvaluator.py
@@ -12,6 +12,8 @@ class InitialStrategyEvaluator(plugins.IStrategyEvaluatorPlugin):
     volumeOfBuys = 0
     sellTotal = 0
     volumeOfSells = 0
+    numberOfBuys = 0
+    numberOfSells = 0
     def __call__(self, trades):
         self.trades = trades
         self.evaluate()
@@ -26,7 +28,7 @@ class InitialStrategyEvaluator(plugins.IStrategyEvaluatorPlugin):
                 total -= amount
                 graph.write(str(total)+"\t"+trade['Time']+"\n")
                 self.buyTotal += amount
-                self.volumeOfBuys+=int(trade['Volume'])                
+                self.volumeOfBuys+=int(trade['Volume'])
             if trade['Seller Broker ID'] == 'Algorithmic':
                 total += amount
                 graph.write(str(-amount)+"\t"+trade['Time']+"\n")
@@ -35,8 +37,8 @@ class InitialStrategyEvaluator(plugins.IStrategyEvaluatorPlugin):
         buyAverage = self.buyTotal/self.volumeOfBuys
         sellAverage = self.sellTotal/self.volumeOfSells
         f = open("Report.txt","w+")
-        f.write('Bought :'+str(self.numberOfBuys)+' shares\n')
-        f.write('Sold :'+str(self.numberOfSells)+' shares\n')
+        f.write('Bought :'+str(self.volumeOfBuys)+' shares\n')
+        f.write('Sold :'+str(self.volumeOfSells)+' shares\n')
         f.write('Profit: $'+str(self.sellTotal-self.buyTotal))
         f.write('Average buy price: $'+buyAverage)
         f.write('Average sell price: $'+sellAverage)
