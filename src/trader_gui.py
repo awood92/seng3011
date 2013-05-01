346fbc999becc29e12cea180780f4bb8a80486be
Nicholas Grasevski <grasevski@gmail.com>
2013-05-01 16:03:12 +1000

    Done toolbar

--- src/trader_gui.py
+++ src/trader_gui.py
@@ -10,15 +10,18 @@ import plugins
 
 
 class MainFrame(wx.Frame):
-    def __init__(self, parent, title):
-        wx.Frame.__init__(self, parent, title=title)
-        self.CreateStatusBar()
-        menuBar = wx.MenuBar()
+    def __init__(self, *args, **kwargs):
+        super(MainFrame, self).__init__(*args, **kwargs)
+        self.InitUI()
+
+    def InitUI(self):
+        self._status_bar = self.CreateStatusBar()
+        self._tool_bar = self.CreateToolBar()
+        menu_bar = wx.MenuBar()
         menu = wx.Menu()
         item = menu.Append(wx.ID_OPEN, '&Open\tCtrl+O',
                            'Open a market data file')
         self.Bind(wx.EVT_MENU, self.OnOpen, item)
-        menu.AppendSeparator()
         item = menu.Append(wx.ID_ANY, '&Export',
                            'Save outputted trades')
         self.Bind(wx.EVT_MENU, self.OnExport, item)
@@ -28,21 +31,44 @@ class MainFrame(wx.Frame):
         item = menu.Append(wx.ID_EXIT, 'E&xit\tCtrl+Q',
                            'Terminate the program')
         self.Bind(wx.EVT_MENU, self.OnExit, item)
-        menuBar.Append(menu, '&File')
+        menu_bar.Append(menu, '&File')
         menu = wx.Menu()
-        item = menu.Append(wx.ID_ANY, '&Initialize',
-                           'Initialize order book')
-        self.Bind(wx.EVT_MENU, self.OnInitialize, item)
+        self._item_status_bar = menu.Append(wx.ID_ANY, 'Show &Statusbar',
+                                            'Toggle visibility of statusbar',
+                                            wx.ITEM_CHECK)
+        self.Bind(wx.EVT_MENU, self.ToggleStatusBar, self._item_status_bar)
+        menu.Check(self._item_status_bar.GetId(), True)
+        self._item_tool_bar = menu.Append(wx.ID_ANY, 'Show &Toolbar',
+                                          'Toggle visibility of toolbar',
+                                          wx.ITEM_CHECK)
+        self.Bind(wx.EVT_MENU, self.ToggleToolBar, self._item_tool_bar)
+        menu.Check(self._item_tool_bar.GetId(), True)
+        menu_bar.Append(menu, '&View')
+        menu = wx.Menu()
         item = menu.Append(wx.ID_ANY, '&Run',
                            'Run trial')
         self.Bind(wx.EVT_MENU, self.OnRun, item)
-        menuBar.Append(menu, '&Tools')
+        menu_bar.Append(menu, '&Tools')
         menu = wx.Menu()
         item = menu.Append(wx.ID_ABOUT, '&About',
                            'Information about this program')
         self.Bind(wx.EVT_MENU, self.OnAbout, item)
-        menuBar.Append(menu, '&Help')
-        self.SetMenuBar(menuBar)
+        menu_bar.Append(menu, '&Help')
+        self.SetMenuBar(menu_bar)
+        icon = wx.ArtProvider.GetBitmap(wx.ART_FILE_OPEN)
+        tool = self._tool_bar.AddLabelTool(wx.ID_OPEN, 'Open', icon)
+        self.Bind(wx.EVT_TOOL, self.OnOpen, tool)
+        icon = wx.ArtProvider.GetBitmap(wx.ART_FILE_SAVE_AS)
+        tool = self._tool_bar.AddLabelTool(wx.ID_ANY, 'Export', icon)
+        self.Bind(wx.EVT_TOOL, self.OnExport, tool)
+        self._tool_bar.AddSeparator()
+        icon = wx.ArtProvider.GetBitmap(wx.ART_CROSS_MARK)
+        tool = self._tool_bar.AddLabelTool(wx.ID_CLOSE, 'Close', icon)
+        self.Bind(wx.EVT_TOOL, self.OnClose, tool)
+        self._tool_bar.AddSeparator()
+        icon = wx.Bitmap('resources/run.png')
+        tool = self._tool_bar.AddLabelTool(wx.ID_ANY, 'Run', icon)
+        self.Bind(wx.EVT_TOOL, self.OnRun, tool)
 
     def OnOpen(self, e):
         wildcard = 'CSV files (*.csv)|*.csv|Text files (*.txt)|*.txt|' \
@@ -58,14 +84,23 @@ class MainFrame(wx.Frame):
     def OnClose(self, e):
         raise NotImplementedError
 
-    def OnInitialize(self, e):
-        raise NotImplementedError
+    def OnExit(self, e):
+        self.Close(True)
+
+    def ToggleStatusBar(self, e):
+        if self._item_status_bar.IsChecked():
+            self._status_bar.Show()
+        else:
+            self._status_bar.Hide()
+
+    def ToggleToolBar(self, e):
+        if self._item_tool_bar.IsChecked():
+            self._tool_bar.Show()
+        else:
+            self._tool_bar.Hide()
 
     def OnRun(self, e):
         raise NotImplementedError
-
-    def OnExit(self, e):
-        self.Close(True)
 
     def OnAbout(self, e):
         dlg = wx.MessageDialog(self, 'Algorithmic Trading System',
@@ -76,7 +111,7 @@ class MainFrame(wx.Frame):
 
 def main():
     app = wx.App(False)
-    MainFrame(None, 'Algorithmic Trading System').Show()
+    MainFrame(None, title='Algorithmic Trading System').Show()
     app.MainLoop()
 
 if __name__ == '__main__':
