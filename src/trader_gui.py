7bb7133dbf0a5d5a873e1e3d9435458095214d00
Nicholas Grasevski <grasevski@gmail.com>
2013-05-02 16:53:39 +1000

    Done basic trader gui

--- src/trader_gui.py
+++ src/trader_gui.py
@@ -113,26 +113,26 @@ class MainFrame(wx.Frame):
         """Open market data file"""
         wildcard = 'CSV files (*.csv)|*.csv|Text files (*.txt)|*.txt|' \
                    'All files (*.*)|*.*'
-        dlg = wx.FileDialog(self, wildcard=wildcard,
-                            style=wx.OPEN|wx.MULTIPLE)
+        dialog = wx.FileDialog(self, wildcard=wildcard,
+                               style=wx.OPEN|wx.MULTIPLE)
         count = self._notebook.GetPageCount()
-        if dlg.ShowModal() == wx.ID_OK:
-            for path in dlg.GetPaths():
+        if dialog.ShowModal() == wx.ID_OK:
+            for path in dialog.GetPaths():
                 panel = TabPanel(self._notebook)
                 panel.Open(path)
                 self._notebook.AddPage(panel, os.path.basename(path), True)
         if count == 0 and self._notebook.GetPageCount() > 0:
             self._enable_controls()
-        dlg.Destroy()
+        dialog.Destroy()
 
     def OnExport(self, e):
         """Save algorithmic trades"""
         wildcard = 'CSV files (*.csv)|*.csv|Text files (*.txt)|*.txt|' \
                    'All files (*.*)|*.*'
-        dlg = wx.FileDialog(self, wildcard=wildcard, style=wx.SAVE)
-        if dlg.ShowModal() == wx.ID_OK:
-            self._notebook.GetCurrentPage().Export(dlg.GetPath())
-        dlg.Destroy()
+        dialog = wx.FileDialog(self, wildcard=wildcard, style=wx.SAVE)
+        if dialog.ShowModal() == wx.ID_OK:
+            self._notebook.GetCurrentPage().Export(dialog.GetPath())
+        dialog.Destroy()
 
     def OnClose(self, e):
         """Close the current market data file"""
@@ -144,10 +144,10 @@ class MainFrame(wx.Frame):
         """Exit the program"""
         close = True
         if self._notebook.GetPageCount() > 0:
-            dlg = wx.MessageDialog(self, 'Close all tabs?', 'Quit')
-            if dlg.ShowModal() != wx.ID_OK:
+            dialog = wx.MessageDialog(self, 'Close all tabs?', 'Quit')
+            if dialog.ShowModal() != wx.ID_OK:
                 close = False
-            dlg.Destroy()
+            dialog.Destroy()
         if close:
             self.Close(True)
 
@@ -171,10 +171,10 @@ class MainFrame(wx.Frame):
 
     def OnAbout(self, e):
         """About the program"""
-        dlg = wx.MessageDialog(self, 'Algorithmic Trading System',
-                               'Algorithmic Trading System', wx.OK)
-        dlg.ShowModal()
-        dlg.Destroy()
+        dialog = wx.MessageDialog(self, 'Algorithmic Trading System',
+                                  'Algorithmic Trading System', wx.OK)
+        dialog.ShowModal()
+        dialog.Destroy()
 
 
 class TabPanel(wx.Panel):
@@ -186,7 +186,72 @@ class TabPanel(wx.Panel):
 
     def InitUI(self):
         """Initialize the workspace"""
-        pass
+        plugin_manager = yapsy.PluginManager.PluginManager()
+        plugin_manager.setPluginPlaces(['plugins'])
+        plugin_manager.setCategoriesFilter({
+            'SignalGenerator': plugins.ISignalGeneratorPlugin,
+            'Engine': plugins.IEnginePlugin,
+            'StrategyEvaluator': plugins.IStrategyEvaluatorPlugin,
+        })
+        plugin_manager.collectPlugins()
+        category_plugins = plugin_manager.getPluginsOfCategory('SignalGenerator')
+        self._signal_generators = {}
+        for plugin in category_plugins:
+            plugin_settings = TabPanel._plugin_settings(plugin)
+            self._signal_generators[plugin.details.get('Core', 'Name')] = plugin_settings
+        category_plugins = plugin_manager.getPluginsOfCategory('Engine')
+        self._engines = {}
+        for plugin in category_plugins:
+            plugin_settings = TabPanel._plugin_settings(plugin)
+            self._engines[plugin.details.get('Core', 'Name')] = plugin_settings
+        category_plugins = plugin_manager.getPluginsOfCategory('StrategyEvaluator')
+        self._strategy_evaluators = {}
+        for plugin in category_plugins:
+            plugin_settings = TabPanel._plugin_settings(plugin)
+            self._strategy_evaluators[plugin.details.get('Core', 'Name')] = plugin_settings
+        grid = wx.FlexGridSizer(3, 4, 3, 3)
+        self._signal_generator = self._add_plugin(grid, 'Signal Generator',
+                                                  self._signal_generators.keys(),
+                                                  self.OnConfigSignalGenerator,
+                                                  self.OnAboutSignalGenerator)
+        self._engine = self._add_plugin(grid, 'Engine',
+                                        self._engines.keys(),
+                                        self.OnConfigEngine,
+                                        self.OnAboutEngine)
+        self._strategy_evaluator = self._add_plugin(grid, 'Strategy Evaluator',
+                                                    self._strategy_evaluators.keys(),
+                                                    self.OnConfigStrategyEvaluator,
+                                                    self.OnAboutStrategyEvaluator)
+        box = wx.BoxSizer()
+        box.Add(grid, 1, wx.EXPAND|wx.ALL, 5)
+        self.SetSizer(box)
+
+    @staticmethod
+    def _plugin_settings(plugin):
+        if plugin.details.has_section('Parameters'):
+            default = dict(plugin.details.items('Parameters'))
+        else:
+            default = {}
+        return {'plugin': plugin, 'default': default}
+
+    def _add_plugin(self, grid, name, choices, on_config, on_about):
+        text = wx.StaticText(self, label=name+':')
+        label = ''
+        if len(choices) > 0:
+            label = choices[0]
+        combo_box = wx.ComboBox(self, wx.ID_ANY, label, choices=choices)
+        config = wx.Button(self, label='Configure')
+        about = wx.Button(self, label='About')
+        self.Bind(wx.EVT_BUTTON, on_config, config)
+        self.Bind(wx.EVT_BUTTON, on_about, about)
+        row = [
+            (text, 1, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL),
+            (combo_box, 1, wx.EXPAND),
+            config,
+            about
+        ]
+        grid.AddMany(row)
+        return combo_box
 
     def Open(self, path):
         """Open the market data file"""
@@ -211,10 +276,132 @@ class TabPanel(wx.Panel):
 
     def Run(self):
         """Run the simulation"""
-        self._trades = trader.run_trial(self._market_data,
-                                        self._signal_generator,
-                                        self._engine,
-                                        self._strategy_evaluator)
+        plugin_name = self._signal_generator.GetValue()
+        plugin = self._signal_generators[plugin_name]['plugin']
+        signal_generator = plugin.plugin_object
+        signal_generator.setup(plugin.details)
+        plugin_name = self._engine.GetValue()
+        plugin = self._engines[plugin_name]['plugin']
+        engine = plugin.plugin_object
+        engine.setup(plugin.details)
+        plugin_name = self._strategy_evaluator.GetValue()
+        plugin = self._strategy_evaluators[plugin_name]['plugin']
+        strategy_evaluator = plugin.plugin_object
+        strategy_evaluator.setup(plugin.details)
+        self._trades = trader.run_trial(self._market_data, signal_generator,
+                                        engine, strategy_evaluator)
+
+    def _config(self, control, plugins):
+        plugin_name = control.GetValue()
+        if plugin_name in plugins:
+            plugin = plugins[plugin_name]
+            if plugin['plugin'].details.has_section('Parameters'):
+                config = dict(plugin['plugin'].details.items('Parameters'))
+            else:
+                config = {}
+            dialog = ConfigDialog(None, title=plugin_name+' Configuration')
+            dialog.InitUI(config, plugin['default'])
+            if dialog.ShowModal() == wx.ID_OK:
+                for name, value in dialog.config.iteritems():
+                    plugin['plugin'].details.set('Parameters', name, value)
+            dialog.Destroy()
+        else:
+            dialog = wx.MessageDialog(self,
+                                      'Please choose a plugin to configure',
+                                      'No plugin chosen', wx.OK)
+            dialog.ShowModal()
+            dialog.Destroy()
+
+    def _about(self, control, plugins):
+        message = 'Please choose a plugin'
+        caption = 'No plugin chosen'
+        plugin_name = control.GetValue()
+        if plugin_name in plugins:
+            plugin = plugins[plugin_name]['plugin']
+            caption = plugin.name
+            message = plugin.description
+        dialog = wx.MessageDialog(self, message, caption, wx.OK)
+        dialog.ShowModal()
+        dialog.Destroy()
+
+    def OnConfigSignalGenerator(self, e):
+        """Show modal configure dialog"""
+        self._config(self._signal_generator, self._signal_generators)
+
+    def OnAboutSignalGenerator(self, e):
+        """Show modal about dialog"""
+        self._about(self._signal_generator, self._signal_generators)
+
+    def OnConfigEngine(self, e):
+        """Show modal configure dialog"""
+        self._config(self._engine, self._engines)
+
+    def OnAboutEngine(self, e):
+        """Show modal about dialog"""
+        self._about(self._engine, self._engines)
+
+    def OnConfigStrategyEvaluator(self, e):
+        """Show modal configure dialog"""
+        self._config(self._strategy_evaluator, self._strategy_evaluators)
+
+    def OnAboutStrategyEvaluator(self, e):
+        """Show modal about dialog"""
+        self._about(self._strategy_evaluator, self._strategy_evaluators)
+
+
+class ConfigDialog(wx.Dialog):
+    """Plugin configuration dialog"""
+
+    def InitUI(self, config, default):
+        """Initialize the dialog"""
+        grid = wx.FlexGridSizer(2, 1, 5, 5)
+        fields = wx.FlexGridSizer(0, 2)
+        self._fields = {}
+        self._config = config
+        self._default = default
+        for name, value in config.iteritems():
+            text = wx.StaticText(self, label=name)
+            self._fields[name] = wx.TextCtrl(self, value=value)
+            row = [
+                (text, 1, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL),
+                (self._fields[name], 1, wx.EXPAND)
+            ]
+            fields.AddMany(row)
+        ok = wx.Button(self, wx.ID_OK)
+        cancel = wx.Button(self, wx.ID_CANCEL)
+        default = wx.Button(self, label='Default')
+        self.Bind(wx.EVT_BUTTON, self.OnOk, ok)
+        self.Bind(wx.EVT_BUTTON, self.OnCancel, cancel)
+        self.Bind(wx.EVT_BUTTON, self.OnDefault, default)
+        buttons = wx.GridSizer(1, 3, 3, 3)
+        row = [
+            (ok, 1, wx.EXPAND),
+            (cancel, 1, wx.EXPAND),
+            (default, 1, wx.EXPAND)
+        ]
+        buttons.AddMany(row)
+        grid.AddMany([fields, buttons])
+        self.SetSizer(grid)
+
+    def OnOk(self, e):
+        """Close the window and return ok"""
+        for name in self._fields:
+            self._config[name] = self._fields[name].GetValue()
+        self.EndModal(wx.ID_OK)
+
+    def OnCancel(self, e):
+        """Close the window and return cancel"""
+        self.EndModal(wx.ID_CANCEL)
+
+    def OnDefault(self, e):
+        """Reset entered values to default"""
+        for name, value in self._default.iteritems():
+            self._fields[name].SetValue(value)
+
+    @property
+    def config(self):
+        """User specified configuration"""
+        return self._config
 
 
 def main():
