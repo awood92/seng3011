c463a3d543124c3f2c8399cb7cd9c902d2326826
George El Boustani <george_el_boustani@hotmail.com>
2013-05-17 02:15:28 +1000

    Added test harness to run all the tests

---
+++ src/TestHarness.py
@@ -0,0 +1,10 @@
+print "Test suite 'TestMeanReversionSignalGenerator':"
+import TestMeanReversionSignalGenerator
+print "------------------------\nTest suite 'TestMomentumSignalGenerator\n------------------------':"
+import TestMomentumSignalGenerator
+print "------------------------\nTest suite 'TestSignalGeneratorUtils\n------------------------':"
+import TestSignalGeneratorUtils
+print "------------------------\nTest suite 'TestOrderbook\n------------------------':"
+import TestOrderbook
+
+print "----------------------------------------------------------\nAll tests passed, but no event b model - does it work :S?\n----------------------------------------------------------"
--- src/TestSignalGeneratorUtils.py
+++ src/TestSignalGeneratorUtils.py
@@ -7,25 +7,21 @@ def testAverageReturnCalculation():
     data = [7,4]
     data_result = -0.42857142857142855
     assert data_result == _calculateAverageReturn(data)
-    print "test passed"
     
     print "for list [1,2,3,4,5,6,7,8,9]"
     data = [1,2,3,4,5,6,7,8,9]
     data_result = 0.3397321428571428
     assert data_result == _calculateAverageReturn(data)
-    print "test passed"
     
     print "for list [7,4,1,6,2,10,2,3,6,6,5,13]"
     data = [7,4,1,6,2,10,2,3,6,6,5,13]
     data_result = 0.8443722943722943
     assert data_result == _calculateAverageReturn(data)
-    print "test passed"
     
-    print "for list [7,4,1,6,2,10,2,3,6,6,5,13]"
+    print "for list [2.5,2.45,2.35,2.2,2.5,2.45,2.35,2.2,2.1,2,1.8]"
     data = [2.5,2.45,2.35,2.2,2.5,2.45,2.35,2.2,2.1,2,1.8]
     data_result = -0.03060021842392663
     assert data_result == _calculateAverageReturn(data)
-    print "test passed"
 
 testAverageReturnCalculation()
     
